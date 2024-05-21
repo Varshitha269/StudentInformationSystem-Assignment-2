ba42a2b2e8bf1dc8b2559b5c9d6bd2e8c66e65f3
Govindaswamy Varshitha <99487978+Varshitha269@users.noreply.github.com>
2024-05-21 13:52:00 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using SIS_Assignment_C_.Main;
+
+namespace SIS_Assignment_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentInformationSystemApp sis = new StudentInformationSystemApp();
+            sis.Run();
+
+        }
+    }
+}
